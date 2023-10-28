6b35a48fd1f50f7b1aabba9fc87fbcf275a137f8
Chris Banes <chris@banes.me>
2023-10-28 17:12:48 +0100

    Update Sample icon

--- sample/android/src/main/kotlin/dev/chrisbanes/haze/sample/android/MainActivity.kt
+++ sample/android/src/main/kotlin/dev/chrisbanes/haze/sample/android/MainActivity.kt
@@ -6,10 +6,12 @@ package dev.chrisbanes.haze.sample.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import dev.chrisbanes.haze.sample.HazeSample
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
     super.onCreate(savedInstanceState)
 
     setContent {
