8808d43f7e05356e6ec86733494e7815e4246419
Chris Banes <chris@banes.me>
2023-10-30 21:03:23 +0000

    Add noise to Android implementation
    Using a precomputed noise texture, which overlays
    the blurred content.

--- haze-jetpack-compose/src/main/kotlin/dev/chrisbanes/haze/Haze.jetpackcompose.kt
+++ haze-jetpack-compose/src/main/kotlin/dev/chrisbanes/haze/Haze.jetpackcompose.kt
@@ -3,13 +3,21 @@
 
 package dev.chrisbanes.haze
 
+import android.graphics.BitmapFactory
+import android.graphics.BitmapShader
 import android.graphics.BlendMode
 import android.graphics.BlendModeColorFilter
-import android.graphics.RenderEffect
+import android.graphics.RenderEffect.createBlendModeEffect
+import android.graphics.RenderEffect.createBlurEffect
+import android.graphics.RenderEffect.createColorFilterEffect
+import android.graphics.RenderEffect.createShaderEffect
 import android.graphics.RenderNode
 import android.graphics.Shader
+import android.graphics.Shader.TileMode.REPEAT
 import android.os.Build
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.geometry.Rect
@@ -20,8 +28,10 @@ import androidx.compose.ui.graphics.drawscope.draw
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import dev.chrisbanes.haze.jetpackcompose.R
 
 fun Modifier.haze(
   vararg area: Rect,
@@ -77,70 +87,90 @@ internal fun Modifier.haze(
     }
   }
 
-  return drawWithCache {
-    // This is our RenderEffect. It first applies a blur effect, and then a color filter effect
-    // to allow content to be visible on top
-    val blurRadiusPx = blurRadius.toPx()
-    val effect = RenderEffect.createColorFilterEffect(
-      BlendModeColorFilter(tint.toArgb(), BlendMode.SRC_OVER),
-      RenderEffect.createBlurEffect(blurRadiusPx, blurRadiusPx, Shader.TileMode.DECAL),
-    )
-
-    val contentNode = RenderNode("content").apply {
-      setPosition(0, 0, size.width.toInt(), size.height.toInt())
+  return composed {
+    val context = LocalContext.current
+    val bitmap = remember(context) {
+      BitmapFactory.decodeResource(context.resources, R.drawable.noise)
     }
 
-    // We create a RenderNode for each of the areas we need to apply our effect to
-    val effectRenderNodes = areas.map { area ->
-      // We expand the area where our effect is applied to. This is necessary so that the blur
-      // effect is applied evenly to allow edges. If we don't do this, the blur effect is much less
-      // visible on the edges of the area.
-      val expandedRect = area.inflate(blurRadiusPx)
+    drawWithCache {
+      // This is our RenderEffect. It first applies a blur effect, and then a color filter effect
+      // to allow content to be visible on top
+      val blurRadiusPx = blurRadius.toPx()
 
-      val node = RenderNode("blur").apply {
-        setRenderEffect(effect)
-        setPosition(0, 0, expandedRect.width.toInt(), expandedRect.height.toInt())
-        translationX = expandedRect.left
-        translationY = expandedRect.top
+      val effect = createBlurEffect(
+        blurRadiusPx,
+        blurRadiusPx,
+        Shader.TileMode.DECAL
+      ).let {
+        createBlendModeEffect(
+          createShaderEffect(BitmapShader(bitmap, REPEAT, REPEAT)),
+          it,
+          BlendMode.OVERLAY,
+        )
+      }.let {
+        createColorFilterEffect(
+          BlendModeColorFilter(tint.toArgb(), BlendMode.SRC_OVER),
+          it,
+        )
       }
-      EffectRenderNodeHolder(renderNode = node, renderNodeDrawArea = expandedRect, area = area)
-    }
 
-    onDrawWithContent {
-      // First we draw the composable content into `contentNode`
-      Canvas(contentNode.beginRecording()).also { canvas ->
-        draw(this, layoutDirection, canvas, size) {
-          this@onDrawWithContent.drawContent()
+      val contentNode = RenderNode("content").apply {
+        setPosition(0, 0, size.width.toInt(), size.height.toInt())
+      }
+
+      // We create a RenderNode for each of the areas we need to apply our effect to
+      val effectRenderNodes = areas.map { area ->
+        // We expand the area where our effect is applied to. This is necessary so that the blur
+        // effect is applied evenly to allow edges. If we don't do this, the blur effect is much less
+        // visible on the edges of the area.
+        val expandedRect = area.inflate(blurRadiusPx)
+
+        val node = RenderNode("blur").apply {
+          setRenderEffect(effect)
+          setPosition(0, 0, expandedRect.width.toInt(), expandedRect.height.toInt())
+          translationX = expandedRect.left
+          translationY = expandedRect.top
         }
-        contentNode.endRecording()
+        EffectRenderNodeHolder(renderNode = node, renderNodeDrawArea = expandedRect, area = area)
       }
 
-      // Now we draw `contentNode` into the window canvas, so that it is displayed
-      drawIntoCanvas { canvas ->
-        canvas.nativeCanvas.drawRenderNode(contentNode)
-      }
-
-      // Now we need to draw `contentNode` into each of our 'effect' RenderNodes, allowing
-      // their RenderEffect to be applied to the composable content.
-      effectRenderNodes.forEach { effect ->
-        effect.renderNode.beginRecording().also { canvas ->
-          // We need to draw our background color first, as the `contentNode` may not draw
-          // a background. This then makes the blur effect much less pronounced, as blurring with
-          // transparent negates the effect.
-          canvas.drawColor(backgroundColor.toArgb())
-          canvas.translate(-effect.renderNodeDrawArea.left, -effect.renderNodeDrawArea.top)
-          canvas.drawRenderNode(contentNode)
-          effect.renderNode.endRecording()
+      onDrawWithContent {
+        // First we draw the composable content into `contentNode`
+        Canvas(contentNode.beginRecording()).also { canvas ->
+          draw(this, layoutDirection, canvas, size) {
+            this@onDrawWithContent.drawContent()
+          }
+          contentNode.endRecording()
         }
-      }
 
-      // Finally we draw each 'effect' RenderNode to the window canvas, drawing on top
-      // of the original content
-      drawIntoCanvas { canvas ->
+        // Now we draw `contentNode` into the window canvas, so that it is displayed
+        drawIntoCanvas { canvas ->
+          canvas.nativeCanvas.drawRenderNode(contentNode)
+        }
+
+        // Now we need to draw `contentNode` into each of our 'effect' RenderNodes, allowing
+        // their RenderEffect to be applied to the composable content.
         effectRenderNodes.forEach { effect ->
-          with(effect) {
-            clipRect(area.left, area.top, area.right, area.bottom) {
-              canvas.nativeCanvas.drawRenderNode(renderNode)
+          effect.renderNode.beginRecording().also { canvas ->
+            // We need to draw our background color first, as the `contentNode` may not draw
+            // a background. This then makes the blur effect much less pronounced, as blurring with
+            // transparent negates the effect.
+            canvas.drawColor(backgroundColor.toArgb())
+            canvas.translate(-effect.renderNodeDrawArea.left, -effect.renderNodeDrawArea.top)
+            canvas.drawRenderNode(contentNode)
+            effect.renderNode.endRecording()
+          }
+        }
+
+        // Finally we draw each 'effect' RenderNode to the window canvas, drawing on top
+        // of the original content
+        drawIntoCanvas { canvas ->
+          effectRenderNodes.forEach { effect ->
+            with(effect) {
+              clipRect(area.left, area.top, area.right, area.bottom) {
+                canvas.nativeCanvas.drawRenderNode(renderNode)
+              }
             }
           }
         }
