fac118412bae0fc1eb9c6139bdc7c97b54b9cd99
Chris Banes <chrisbanes@users.noreply.github.com>
2023-11-09 22:00:13 +0000

    Fix JVM targets (#28)
    - Build with JDK 20
    - Target JVM 11 bytecode

--- gradle/build-logic/convention/src/main/kotlin/dev/chrisbanes/gradle/Kotlin.kt
+++ gradle/build-logic/convention/src/main/kotlin/dev/chrisbanes/gradle/Kotlin.kt
@@ -4,8 +4,23 @@
 package dev.chrisbanes.gradle
 
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompilerOptions
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 
 fun Project.configureKotlin() {
   // Configure Java to use our chosen language level. Kotlin will automatically pick this up
   configureJava()
+
+  tasks.withType<KotlinCompilationTask<*>>().configureEach {
+    compilerOptions {
+      allWarningsAsErrors.set(true)
+
+      if (this is KotlinJvmCompilerOptions) {
+        // Target JVM 11 bytecode
+        jvmTarget.set(JvmTarget.JVM_11)
+      }
+    }
+  }
 }
