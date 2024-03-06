e9cb4243f4142df19fe3583db3fe346deab998f2
Chris Banes <chris@banes.me>
2024-03-06 21:08:50 +0000

    Add wasm support

--- gradle/build-logic/convention/src/main/kotlin/dev/chrisbanes/gradle/KotlinMultiplatformConventionPlugin.kt
+++ gradle/build-logic/convention/src/main/kotlin/dev/chrisbanes/gradle/KotlinMultiplatformConventionPlugin.kt
@@ -8,6 +8,7 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 
 @OptIn(ExperimentalKotlinGradlePluginApi::class)
 class KotlinMultiplatformConventionPlugin : Plugin<Project> {
@@ -29,6 +30,11 @@ class KotlinMultiplatformConventionPlugin : Plugin<Project> {
       iosX64()
       iosArm64()
       iosSimulatorArm64()
+
+      @OptIn(ExperimentalWasmDsl::class)
+      wasmJs {
+        browser()
+      }
 
       configureSpotless()
       configureKotlin()
