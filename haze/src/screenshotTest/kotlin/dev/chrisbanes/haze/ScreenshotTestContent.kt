4cf1d95bc05ba38fefae38cf48e8c11d46e9cb1c
Chris Banes <chris@banes.me>
2024-07-17 17:49:43 +0300

    Throw error when background color is not specified

--- haze/src/screenshotTest/kotlin/dev/chrisbanes/haze/ScreenshotTestContent.kt
+++ haze/src/screenshotTest/kotlin/dev/chrisbanes/haze/ScreenshotTestContent.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -39,6 +40,7 @@ internal fun CreditCardSample(
         .haze(
           state = hazeState,
           style = HazeDefaults.style(
+            backgroundColor = MaterialTheme.colorScheme.background,
             tint = defaultTint,
             blurRadius = 8.dp,
           ),
