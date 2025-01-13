4d408e0a082d7c1c0a36000414b626de4b8ee7cb
Chris Banes <chris@banes.me>
2025-01-13 11:16:43 +0000

    Fix issues with missing effects in LazyLayouts
    Caused by the HazeState changing, due to the remembered
    HazeState now always being remembered across re-used
    Lazy item compositions. We assumed that they always were.
    
    Fixed by properly handling the HazeState changing.

--- haze/src/commonTest/kotlin/dev/chrisbanes/haze/HazeTest.kt
+++ haze/src/commonTest/kotlin/dev/chrisbanes/haze/HazeTest.kt
@@ -5,13 +5,18 @@ package dev.chrisbanes.haze
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.runComposeUiTest
 import assertk.assertFailure
 import assertk.assertThat
+import assertk.assertions.hasSize
+import assertk.assertions.isEmpty
 import assertk.assertions.isFalse
 import kotlin.test.Test
 
@@ -46,5 +51,33 @@ class HazeTest {
         }
       }
     }
+  }
+
+  @Test
+  fun testChangingHazeState() = runComposeUiTest {
+    val hazeState1 = HazeState()
+    val hazeState2 = HazeState()
+
+    val selectedHazeState = mutableStateOf(hazeState1)
+
+    setContent {
+      Box(Modifier.hazeSource(selectedHazeState.value)) {
+        Spacer(
+          Modifier.hazeEffect(selectedHazeState.value, HazeDefaults.style(Color.Blue)),
+        )
+      }
+    }
+
+    // Assert that the HazeArea is in hazeState1
+    assertThat(hazeState1.areas).hasSize(1)
+    assertThat(hazeState2.areas).isEmpty()
+
+    // Update the selected HazeState and wait for idle
+    selectedHazeState.value = hazeState2
+    waitForIdle()
+
+    // Assert that the HazeArea moved to hazeState2
+    assertThat(hazeState1.areas).isEmpty()
+    assertThat(hazeState2.areas).hasSize(1)
   }
 }
