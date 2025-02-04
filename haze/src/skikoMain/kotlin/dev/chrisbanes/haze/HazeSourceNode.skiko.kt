bdfc2a731f29fe2c1d1ee4d459e9771059ebd33a
Chris Banes <chris@banes.me>
2025-02-04 08:19:17 +0000

    Move Lifecycle usage to androidMain

---
+++ haze/src/skikoMain/kotlin/dev/chrisbanes/haze/HazeSourceNode.skiko.kt
@@ -0,0 +1,6 @@
+// Copyright 2025, Christopher Banes and the Haze project contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package dev.chrisbanes.haze
+
+internal actual fun HazeSourceNode.clearHazeAreaLayerOnStop() = Unit
