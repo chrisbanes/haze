cdded29032e843ea446681fcbedc9e81be134ffb
Chris Banes <chrisbanes@users.noreply.github.com>
2025-02-17 21:21:45 +0000

    Bump Android Compile SDK to 35 (#512)

--- gradle/build-logic/convention/src/main/kotlin/dev/chrisbanes/gradle/Versions.kt
+++ gradle/build-logic/convention/src/main/kotlin/dev/chrisbanes/gradle/Versions.kt
@@ -4,7 +4,7 @@
 package dev.chrisbanes.gradle
 
 object Versions {
-  const val COMPILE_SDK = 34
+  const val COMPILE_SDK = 35
   const val MIN_SDK = 21
-  const val TARGET_SDK = 34
+  const val TARGET_SDK = 35
 }
