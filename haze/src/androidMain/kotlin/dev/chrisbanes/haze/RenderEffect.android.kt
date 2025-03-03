chrisbanes/haze

commit 9f7d56548b0256213320ba4da1fc92b060566ad9

Re-introduce expanded layer size (#522)
This PR introduces the expanded layer size, which was removed in #461. 

The key difference in this implementation is that we now no longer
expand the layer size beyond what the content bounds. This means that
the edges are now either blurred using the expanded content, or the
clamp edge treatment. The previous implementation would not allow the
clamp edge treatment to work.

Fixes #516

haze/src/androidMain/kotlin/dev/chrisbanes/haze/RenderEffect.android.kt
@@ -16,9 +16,11 @@ import android.graphics.Shader
 import android.graphics.Shader.TileMode.REPEAT
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.geometry.toRect
+import androidx.compose.ui.geometry.isSpecified
+import androidx.compose.ui.geometry.isUnspecified
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RenderEffect
@@ -40,7 +42,7 @@ internal actual fun CompositionLocalConsumerModifierNode.createRenderEffect(para
 
   require(params.blurRadius >= 0.dp) { "blurRadius needs to be equal or greater than 0.dp" }
 
-  val progressiveShader = params.progressive?.asBrush()?.toShader(params.contentSize)
+  val progressiveShader = params.progressive?.asBrush()?.toShader(params.contentBounds.size)
 
   val blur = when {
     params.blurRadius <= 0.dp -> AndroidRenderEffect.createOffsetEffect(0f, 0f)
@@ -50,7 +52,7 @@ internal actual fun CompositionLocalConsumerModifierNode.createRenderEffect(para
       // our custom blur via a runtime shader
       createBlurImageFilterWithMask(
         blurRadiusPx = with(currentValueOf(LocalDensity)) { params.blurRadius.toPx() },
-        bounds = params.contentSize.toRect(),
+        bounds = params.contentBounds,
         mask = progressiveShader,
       )
     }
@@ -71,8 +73,8 @@ internal actual fun CompositionLocalConsumerModifierNode.createRenderEffect(para
 
   return blur
     .withNoise(currentValueOf(LocalContext), params.noiseFactor)
-    .withTints(params.tints, params.contentSize, params.tintAlphaModulate, progressiveShader)
-    .withMask(params.mask, params.contentSize)
+    .withTints(params.tints, params.contentBounds, params.tintAlphaModulate, progressiveShader)
+    .withMask(params.mask, params.contentBounds)
     .asComposeRenderEffect()
 }
 
@@ -118,11 +120,15 @@ private fun AndroidRenderEffect.withNoise(
 @RequiresApi(31)
 private fun AndroidRenderEffect.withMask(
   mask: Brush?,
-  size: Size,
+  contentBounds: Rect,
   blendMode: BlendMode = BlendMode.DST_IN,
 ): AndroidRenderEffect {
-  val shader = mask?.toShader(size) ?: return this
-  return blendWith(AndroidRenderEffect.createShaderEffect(shader), blendMode)
+  val shader = mask?.toShader(contentBounds.size) ?: return this
+  return blendWith(
+    foreground = AndroidRenderEffect.createShaderEffect(shader),
+    blendMode = blendMode,
+    offset = contentBounds.topLeft,
+  )
 }
 
 private fun Brush.toShader(size: Size): Shader? = when (this) {
@@ -133,23 +139,23 @@ private fun Brush.toShader(size: Size): Shader? = when (this) {
 @RequiresApi(31)
 private fun AndroidRenderEffect.withTints(
   tints: List<HazeTint>,
-  size: Size,
+  contentBounds: Rect,
   alphaModulate: Float = 1f,
-  mask: Shader?,
+  mask: Shader? = null,
 ): AndroidRenderEffect = tints.fold(this) { acc, tint ->
-  acc.withTint(tint, size, alphaModulate, mask)
+  acc.withTint(tint, contentBounds, alphaModulate, mask)
 }
 
 @RequiresApi(31)
 private fun AndroidRenderEffect.withTint(
   tint: HazeTint,
-  size: Size,
+  contentBounds: Rect,
   alphaModulate: Float = 1f,
-  mask: Shader?,
+  mask: Shader? = null,
 ): AndroidRenderEffect {
   if (!tint.isSpecified) return this
 
-  val tintBrush = tint.brush?.toShader(size)
+  val tintBrush = tint.brush?.toShader(contentBounds.size)
   if (tintBrush != null) {
     val brushEffect = when {
       alphaModulate >= 1f -> {
@@ -166,17 +172,19 @@ private fun AndroidRenderEffect.withTint(
 
     return if (mask != null) {
       blendWith(
-        AndroidRenderEffect.createBlendModeEffect(
+        foreground = AndroidRenderEffect.createBlendModeEffect(
           AndroidRenderEffect.createShaderEffect(mask),
           brushEffect,
           BlendMode.SRC_IN,
         ),
         blendMode = tint.blendMode.toAndroidBlendMode(),
+        offset = contentBounds.topLeft,
       )
     } else {
       blendWith(
         foreground = brushEffect,
         blendMode = tint.blendMode.toAndroidBlendMode(),
+        offset = contentBounds.topLeft,
       )
     }
   }
@@ -209,7 +217,20 @@ private fun AndroidRenderEffect.withTint(
 private fun AndroidRenderEffect.blendWith(
   foreground: AndroidRenderEffect,
   blendMode: BlendMode,
-): AndroidRenderEffect = AndroidRenderEffect.createBlendModeEffect(this, foreground, blendMode)
+  offset: Offset = Offset.Zero,
+): AndroidRenderEffect = AndroidRenderEffect.createBlendModeEffect(
+  /* dst */
+  this,
+  /* src */
+  when {
+    offset.isUnspecified -> foreground
+    offset == Offset.Zero -> foreground
+    // We need to offset the shader to the bounds
+    else -> AndroidRenderEffect.createOffsetEffect(offset.x, offset.y, foreground)
+  },
+  /* blendMode */
+  blendMode,
+)
 
 @RequiresApi(33)
 private fun createBlurImageFilterWithMask(

haze/src/skikoMain/kotlin/dev/chrisbanes/haze/RenderEffect.skiko.kt
@@ -6,7 +6,8 @@ package dev.chrisbanes.haze
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.geometry.toRect
+import androidx.compose.ui.geometry.isSpecified
+import androidx.compose.ui.geometry.isUnspecified
 import androidx.compose.ui.graphics.BlurEffect
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -14,7 +15,6 @@ import androidx.compose.ui.graphics.RenderEffect
 import androidx.compose.ui.graphics.ShaderBrush
 import androidx.compose.ui.graphics.asComposeRenderEffect
 import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.isSpecified
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.node.CompositionLocalConsumerModifierNode
 import androidx.compose.ui.node.currentValueOf
@@ -38,20 +38,19 @@ internal actual fun CompositionLocalConsumerModifierNode.createRenderEffect(para
     child("noise", NOISE_SHADER)
   }
 
-  val progressiveShader = params.progressive?.asBrush()?.toShader(params.contentSize)
+  val blurRadiusPx = with(currentValueOf(LocalDensity)) { params.blurRadius.toPx() }
+
+  val progressiveShader = params.progressive?.asBrush()?.toShader(params.contentBounds.size)
   val blur = if (progressiveShader != null) {
     // If we've been provided with a progressive/gradient blur shader, we need to use
     // our custom blur via a runtime shader
     createBlurImageFilterWithMask(
-      blurRadiusPx = with(currentValueOf(LocalDensity)) { params.blurRadius.toPx() },
-      bounds = params.contentSize.toRect(),
+      blurRadiusPx = blurRadiusPx,
+      bounds = params.contentBounds,
       mask = progressiveShader,
     )
   } else {
-    createBlurImageFilter(
-      blurRadiusPx = with(currentValueOf(LocalDensity)) { params.blurRadius.toPx() },
-      bounds = params.contentSize.toRect(),
-    )
+    createBlurImageFilter(blurRadiusPx = blurRadiusPx)
   }
 
   return ImageFilter
@@ -60,8 +59,8 @@ internal actual fun CompositionLocalConsumerModifierNode.createRenderEffect(para
       shaderNames = arrayOf("content", "blur"),
       inputs = arrayOf(null, blur),
     )
-    .withTints(params.tints, params.tintAlphaModulate, params.contentSize, progressiveShader)
-    .withMask(params.mask, params.contentSize)
+    .withTints(params.tints, params.contentBounds, params.tintAlphaModulate, progressiveShader)
+    .withMask(params.mask, params.contentBounds)
     .asComposeRenderEffect()
 }
 
@@ -71,22 +70,22 @@ private fun ImageFilter.chainWith(imageFilter: ImageFilter): ImageFilter {
 
 private fun ImageFilter.withTints(
   tints: List<HazeTint>,
+  contentBounds: Rect,
   alphaModulate: Float = 1f,
-  size: Size,
-  mask: Shader?,
+  mask: Shader? = null,
 ): ImageFilter = tints.fold(this) { acc, tint ->
-  acc.withTint(tint, alphaModulate, size, mask)
+  acc.withTint(tint, contentBounds, alphaModulate, mask)
 }
 
 private fun ImageFilter.withTint(
   tint: HazeTint,
+  contentBounds: Rect,
   alphaModulate: Float = 1f,
-  size: Size,
   mask: Shader?,
 ): ImageFilter {
   if (!tint.isSpecified) return this
 
-  val tintBrush = tint.brush?.toShader(size)
+  val tintBrush = tint.brush?.toShader(contentBounds.size)
   if (tintBrush != null) {
     val brushEffect = when {
       alphaModulate >= 1f -> {
@@ -111,11 +110,13 @@ private fun ImageFilter.withTint(
           crop = null,
         ),
         blendMode = tint.blendMode.toSkiaBlendMode(),
+        offset = contentBounds.topLeft,
       )
     } else {
       blendWith(
         foreground = brushEffect,
         blendMode = tint.blendMode.toSkiaBlendMode(),
+        offset = contentBounds.topLeft,
       )
     }
   }
@@ -133,6 +134,7 @@ private fun ImageFilter.withTint(
           crop = null,
         ),
         blendMode = tint.blendMode.toSkiaBlendMode(),
+        offset = contentBounds.topLeft,
       )
     } else {
       ImageFilter.makeColorFilter(
@@ -148,12 +150,15 @@ private fun ImageFilter.withTint(
 
 private fun ImageFilter.withMask(
   brush: Brush?,
-  size: Size,
-  offset: Offset = Offset.Zero,
+  contentBounds: Rect,
   blendMode: BlendMode = BlendMode.DST_IN,
 ): ImageFilter {
-  val shader = brush?.toShader(size) ?: return this
-  return blendWith(ImageFilter.makeShader(shader, crop = null), blendMode, offset)
+  val shader = brush?.toShader(contentBounds.size) ?: return this
+  return blendWith(
+    foreground = ImageFilter.makeShader(shader, crop = null),
+    blendMode = blendMode,
+    offset = contentBounds.topLeft,
+  )
 }
 
 private fun ImageFilter.blendWith(
@@ -162,18 +167,23 @@ private fun ImageFilter.blendWith(
   offset: Offset = Offset.Zero,
 ): ImageFilter = ImageFilter.makeBlend(
   blendMode = blendMode,
-  fg = ImageFilter.makeOffset(offset.x, offset.y, foreground, crop = null),
+  fg = when {
+    offset.isUnspecified -> foreground
+    offset == Offset.Zero -> foreground
+    // We need to offset the shader to the bounds
+    else -> ImageFilter.makeOffset(offset.x, offset.y, foreground, crop = null)
+  },
   bg = this,
   crop = null,
 )
 
-private fun createBlurImageFilter(blurRadiusPx: Float, bounds: Rect): ImageFilter {
+private fun createBlurImageFilter(blurRadiusPx: Float, bounds: Rect? = null): ImageFilter {
   val sigma = BlurEffect.convertRadiusToSigma(blurRadiusPx)
   return ImageFilter.makeBlur(
     sigmaX = sigma,
     sigmaY = sigma,
     mode = FilterTileMode.CLAMP,
-    crop = bounds.toIRect(),
+    crop = bounds?.toIRect(),
   )
 }
 
@@ -199,10 +209,7 @@ private fun createBlurImageFilterWithMask(
   return shader(vertical = false).chainWith(shader(vertical = true))
 }
 
-private fun Brush.toShader(size: Size): Shader? = when (this) {
-  is ShaderBrush -> createShader(size)
-  else -> null
-}
+private fun Brush.toShader(size: Size): Shader? = (this as? ShaderBrush)?.createShader(size)
 
 private fun Rect.toIRect(): IRect =
   IRect.makeLTRB(left.toInt(), top.toInt(), right.toInt(), bottom.toInt())

haze/src/commonMain/kotlin/dev/chrisbanes/haze/HazeEffectNode.kt
@@ -42,6 +42,7 @@ import androidx.compose.ui.node.ObserverModifierNode
 import androidx.compose.ui.node.currentValueOf
 import androidx.compose.ui.node.invalidateDraw
 import androidx.compose.ui.node.observeReads
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalGraphicsContext
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -131,13 +132,31 @@ class HazeEffectNode(
     }
 
   private val isValid: Boolean
-    get() = size.isSpecified && positionOnScreen.isSpecified && areas.isNotEmpty()
+    get() = size.isSpecified && layerSize.isSpecified && areas.isNotEmpty()
 
   internal var size: Size = Size.Unspecified
     set(value) {
       if (value != field) {
         log(TAG) { "size changed. Current: $field. New: $value" }
         dirtyTracker += DirtyFields.Size
+        field = value
+      }
+    }
+
+  private var layerSize: Size = Size.Unspecified
+    set(value) {
+      if (value != field) {
+        log(TAG) { "layerSize changed. Current: $field. New: $value" }
+        dirtyTracker += DirtyFields.LayerSize
+        field = value
+      }
+    }
+
+  internal var layerOffset: Offset = Offset.Zero
+    set(value) {
+      if (value != field) {
+        log(TAG) { "layerOffset changed. Current: $field. New: $value" }
+        dirtyTracker += DirtyFields.LayerOffset
         field = value
       }
     }
@@ -266,7 +285,32 @@ class HazeEffectNode(
   private fun onPositioned(coordinates: LayoutCoordinates, source: String) {
     positionOnScreen = coordinates.positionForHaze()
     size = coordinates.size.toSize()
-    log(TAG) { "$source: positionOnScreen=$positionOnScreen, size=$size" }
+
+    val blurRadiusPx = with(currentValueOf(LocalDensity)) {
+      resolveBlurRadius().takeOrElse { 0.dp }.toPx()
+    }
+
+    val bounds = Rect(positionOnScreen, size)
+
+    // The rect which covers all areas
+    val areasRect = state.areas.fold(Rect.Zero) { acc, area ->
+      acc.expandToInclude(area.bounds ?: Rect.Zero)
+    }
+
+    // Now we clip the expanded layer bounds, to remove anything areas which
+    // don't overlap any areas
+    val clippedLayerBounds = bounds.inflate(blurRadiusPx).intersect(areasRect)
+
+    layerSize = clippedLayerBounds.size
+    layerOffset = positionOnScreen - clippedLayerBounds.topLeft
+
+    log(TAG) {
+      "$source: positionOnScreen=$positionOnScreen, " +
+        "size=$size, " +
+        "layerSize=$layerSize, " +
+        "layerOffset=$layerOffset"
+    }
+
     updateEffect()
   }
 
@@ -334,19 +378,21 @@ class HazeEffectNode(
     // content around the edges to keep the blurring uniform. Without the extra border,
     // the blur will naturally fade out at the edges.
     val scaleFactor = calculateInputScaleFactor()
-    val scaledSize = size * scaleFactor
+    val inflatedSize = layerSize * scaleFactor
+    // This is the topLeft in the inflated bounds where the real are should be at [0,0]
+    val inflatedOffset = layerOffset
 
     val bg = resolveBackgroundColor()
     require(bg.isSpecified) { "backgroundColor not specified. Please provide a color." }
 
     val bounds = Rect(positionOnScreen, size)
 
-    layer.record(scaledSize.roundToIntSize()) {
+    layer.record(inflatedSize.roundToIntSize()) {
       drawRect(bg)
 
       clipRect {
         scale(scale = scaleFactor, pivot = Offset.Zero) {
-          translate(offset = -positionOnScreen) {
+          translate(inflatedOffset - positionOnScreen) {
             for (area in areas) {
               require(!area.contentDrawing) {
                 "Modifier.haze nodes can not draw Modifier.hazeChild nodes. " +
@@ -382,24 +428,26 @@ class HazeEffectNode(
     }
 
     clipRect {
-      scale(1f / scaleFactor, Offset.Zero) {
-        val p = progressive
-        if (p != null) {
-          drawProgressiveEffect(
-            drawScope = this,
-            progressive = p,
-            contentLayer = layer,
-          )
-        } else {
-          // First make sure that the RenderEffect is updated (if necessary)
-          updateRenderEffectIfDirty()
+      translate(-inflatedOffset) {
+        scale(1f / scaleFactor, Offset.Zero) {
+          val p = progressive
+          if (p != null) {
+            drawProgressiveEffect(
+              drawScope = this,
+              progressive = p,
+              contentLayer = layer,
+            )
+          } else {
+            // First make sure that the RenderEffect is updated (if necessary)
+            updateRenderEffectIfDirty()
 
-          layer.renderEffect = renderEffect
-          layer.alpha = alpha
+            layer.renderEffect = renderEffect
+            layer.alpha = alpha
 
-          // Since we included a border around the content, we need to translate so that
-          // we don't see it (but it still affects the RenderEffect)
-          drawLayer(layer)
+            // Since we included a border around the content, we need to translate so that
+            // we don't see it (but it still affects the RenderEffect)
+            drawLayer(layer)
+          }
         }
       }
     }
@@ -630,7 +678,7 @@ internal class RenderEffectParams(
   val noiseFactor: Float,
   val tints: List<HazeTint> = emptyList(),
   val tintAlphaModulate: Float = 1f,
-  val contentSize: Size,
+  val contentBounds: Rect,
   val mask: Brush? = null,
   val progressive: HazeProgressive? = null,
 )
@@ -663,6 +711,7 @@ internal fun HazeEffectNode.getOrCreateRenderEffect(
   tints: List<HazeTint> = resolveTints(),
   tintAlphaModulate: Float = 1f,
   contentSize: Size = this.size * inputScale,
+  contentOffset: Offset = this.layerOffset * inputScale,
   mask: Brush? = this.mask,
   progressive: HazeProgressive? = null,
 ): RenderEffect? = getOrCreateRenderEffect(
@@ -671,7 +720,7 @@ internal fun HazeEffectNode.getOrCreateRenderEffect(
     noiseFactor = noiseFactor,
     tints = tints,
     tintAlphaModulate = tintAlphaModulate,
-    contentSize = contentSize,
+    contentBounds = Rect(contentOffset, contentSize),
     mask = mask,
     progressive = progressive,
   ),
@@ -745,11 +794,15 @@ internal object DirtyFields {
   const val Alpha = FallbackTint shl 1
   const val Progressive = Alpha shl 1
   const val Areas = Progressive shl 1
+  const val LayerSize = Areas shl 1
+  const val LayerOffset = LayerSize shl 1
 
   const val RenderEffectAffectingFlags =
     BlurEnabled or
       InputScale or
       Size or
+      LayerSize or
+      LayerOffset or
       BlurRadius or
       NoiseFactor or
       Mask or
@@ -763,6 +816,8 @@ internal object DirtyFields {
       InputScale or
       AreaOffsets or
       Size or
+      LayerSize or
+      LayerOffset or
       BackgroundColor or
       Progressive or // TODO: only on Android SDK 32-33
       Areas or
@@ -775,6 +830,8 @@ internal object DirtyFields {
       if (ScreenPosition in dirtyTracker) add("ScreenPosition")
       if (AreaOffsets in dirtyTracker) add("RelativePosition")
       if (Size in dirtyTracker) add("Size")
+      if (LayerSize in dirtyTracker) add("LayerSize")
+      if (LayerOffset in dirtyTracker) add("LayerOffset")
       if (BlurRadius in dirtyTracker) add("BlurRadius")
       if (NoiseFactor in dirtyTracker) add("NoiseFactor")
       if (Mask in dirtyTracker) add("Mask")

haze/src/commonMain/kotlin/dev/chrisbanes/haze/Utils.kt
@@ -6,10 +6,13 @@
 package dev.chrisbanes.haze
 
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.geometry.takeOrElse
 import androidx.compose.ui.layout.LayoutCoordinates
 import kotlin.math.hypot
+import kotlin.math.max
+import kotlin.math.min
 
 internal fun calculateLength(
   start: Offset,
@@ -37,3 +40,10 @@ internal inline fun <T> unsynchronizedLazy(noinline initializer: () -> T): Lazy<
 }
 
 internal expect fun LayoutCoordinates.positionForHaze(): Offset
+
+internal fun Rect.expandToInclude(other: Rect): Rect = Rect(
+  left = min(left, other.left),
+  top = min(top, other.top),
+  right = max(right, other.right),
+  bottom = max(bottom, other.bottom),
+)

sample/shared/src/commonMain/kotlin/dev/chrisbanes/haze/sample/ListOverImage.kt
@@ -3,14 +3,13 @@
 
 package dev.chrisbanes.haze.sample
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Refresh
@@ -28,7 +27,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
@@ -77,7 +75,6 @@ fun ListOverImage(navigator: Navigator) {
         )
 
         LazyColumn(
-          verticalArrangement = Arrangement.spacedBy(8.dp),
           contentPadding = contentPadding,
           modifier = Modifier
             .testTag("lazy_column")
@@ -95,9 +92,8 @@ fun ListOverImage(navigator: Navigator) {
             ) {
               Box(
                 modifier = Modifier
-                  .fillMaxSize(0.8f)
-                  .align(Alignment.Center)
-                  .clip(RoundedCornerShape(4.dp))
+                  .fillMaxSize()
+                  .padding(horizontal = 24.dp)
                   .hazeEffect(state = hazeState, style = HazeMaterials.thin()),
               ) {
                 Text(

commit d35a6ce59b741b3347373be075cc9bc8bd879192

Fix progressive effect being drawn incorrectly  (#538)
Regression from #522. Only affects Android.

Fixes #536

haze/src/androidMain/kotlin/dev/chrisbanes/haze/RenderEffect.android.kt
@@ -19,7 +19,6 @@ import androidx.annotation.RequiresApi
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.geometry.isSpecified
 import androidx.compose.ui.geometry.isUnspecified
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -72,7 +71,7 @@ internal actual fun CompositionLocalConsumerModifierNode.createRenderEffect(para
   }
 
   return blur
-    .withNoise(currentValueOf(LocalContext), params.noiseFactor)
+    .withNoise(currentValueOf(LocalContext), params.noiseFactor, progressiveShader)
     .withTints(params.tints, params.contentBounds, params.tintAlphaModulate, progressiveShader)
     .withMask(params.mask, params.contentBounds)
     .asComposeRenderEffect()
@@ -104,11 +103,25 @@ private fun Context.getNoiseTexture(noiseFactor: Float): Bitmap {
 private fun AndroidRenderEffect.withNoise(
   context: Context,
   noiseFactor: Float,
+  mask: Shader? = null,
 ): AndroidRenderEffect = when {
   noiseFactor >= 0.005f -> {
     val noiseShader = BitmapShader(context.getNoiseTexture(noiseFactor), REPEAT, REPEAT)
+    val dst = when {
+      mask != null -> {
+        // If we have a mask, we need to apply it to the noise bitmap shader via a
+        // blend mode
+        AndroidRenderEffect.createBlendModeEffect(
+          AndroidRenderEffect.createShaderEffect(mask), // dst
+          AndroidRenderEffect.createShaderEffect(noiseShader), // src
+          BlendMode.SRC_IN, // blendMode
+        )
+      }
+      else -> AndroidRenderEffect.createShaderEffect(noiseShader)
+    }
+
     AndroidRenderEffect.createBlendModeEffect(
-      AndroidRenderEffect.createShaderEffect(noiseShader), // dst
+      dst, // dst
       this, // src
       BlendMode.DST_ATOP, // blendMode
     )
@@ -161,6 +174,7 @@ private fun AndroidRenderEffect.withTint(
       alphaModulate >= 1f -> {
         AndroidRenderEffect.createShaderEffect(tintBrush)
       }
+
       else -> {
         // If we need to modulate the alpha, we'll need to wrap it in a ColorFilter
         AndroidRenderEffect.createColorFilterEffect(
@@ -201,6 +215,7 @@ private fun AndroidRenderEffect.withTint(
           AndroidRenderEffect.createShaderEffect(mask),
         ),
         blendMode = tint.blendMode.toAndroidBlendMode(),
+        offset = contentBounds.topLeft,
       )
     } else {
       AndroidRenderEffect.createColorFilterEffect(
@@ -217,7 +232,7 @@ private fun AndroidRenderEffect.withTint(
 private fun AndroidRenderEffect.blendWith(
   foreground: AndroidRenderEffect,
   blendMode: BlendMode,
-  offset: Offset = Offset.Zero,
+  offset: Offset,
 ): AndroidRenderEffect = AndroidRenderEffect.createBlendModeEffect(
   /* dst */
   this,

haze/src/commonMain/kotlin/dev/chrisbanes/haze/HazeShaders.kt
@@ -81,7 +81,10 @@ private fun makeBlurSksl(vertical: Boolean): String = """
   }
 
   vec4 main(vec2 coord) {
-    float intensity = mask.eval(coord).a;
+    // Offset the coord for the mask, but coerce it to be at least 0, 0
+    vec2 maskCoord = max(coord - crop.xy, vec2(0.0, 0.0));
+    float intensity = mask.eval(maskCoord).a;
+
     return blur(coord, mix(0.0, blurRadius, intensity));
   }
 """

haze/src/skikoMain/kotlin/dev/chrisbanes/haze/RenderEffect.skiko.kt
@@ -164,7 +164,7 @@ private fun ImageFilter.withMask(
 private fun ImageFilter.blendWith(
   foreground: ImageFilter,
   blendMode: BlendMode,
-  offset: Offset = Offset.Zero,
+  offset: Offset,
 ): ImageFilter = ImageFilter.makeBlend(
   blendMode = blendMode,
   fg = when {