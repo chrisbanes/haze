f5dc79add44818cb16d0fa93b96195b0bf732f1a
Chris Banes <chris@banes.me>
2025-03-05 19:21:38 +0000

    Remove usage of removeFirst

--- haze/src/commonMain/kotlin/dev/chrisbanes/haze/Pool.kt
+++ haze/src/commonMain/kotlin/dev/chrisbanes/haze/Pool.kt
@@ -6,7 +6,7 @@ package dev.chrisbanes.haze
 internal open class Pool<T>(private val maxSize: Int) {
   private val pool = mutableListOf<T>()
 
-  fun get(): T? = pool.removeFirstOrNull()
+  fun get(): T? = if (pool.isNotEmpty()) pool.removeAt(0) else null
 
   fun release(instance: T) {
     pool.add(instance)
@@ -15,7 +15,7 @@ internal open class Pool<T>(private val maxSize: Int) {
 
   private fun maintainSize() {
     while (pool.size > maxSize) {
-      pool.removeFirst()
+      pool.removeAt(0)
     }
   }
 }
