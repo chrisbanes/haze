001ba7658a80dcde2a21be395099dcdef15a55cf
Chris Banes <chris@banes.me>
2025-03-23 10:25:37 +0000

    Fix flicker

--- haze/src/androidMain/kotlin/dev/chrisbanes/haze/HazeSourceNode.android.kt
+++ haze/src/androidMain/kotlin/dev/chrisbanes/haze/HazeSourceNode.android.kt
@@ -3,15 +3,19 @@
 
 package dev.chrisbanes.haze
 
+import android.content.Context
+import android.content.ContextWrapper
+import androidx.activity.ComponentActivity
 import androidx.compose.ui.node.currentValueOf
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.compose.LocalLifecycleOwner
 import kotlinx.coroutines.launch
 
 internal actual fun HazeSourceNode.clearHazeAreaLayerOnStop() {
-  val lifecycleOwner = currentValueOf(LocalLifecycleOwner)
+  // TODO: Move to LocalActivity in Compose 1.8.0
+  val activity = currentValueOf(LocalContext).findActivityOrNull() ?: return
   coroutineScope.launch {
-    lifecycleOwner.lifecycle.currentStateFlow.collect { state ->
+    activity.lifecycle.currentStateFlow.collect { state ->
       if (state <= Lifecycle.State.CREATED) {
         // When the UI host is stopped, release the GraphicsLayer. Android seems to have a issue
         // tracking layers re-paints after an Activity stop + start. Clearing the layer once
@@ -20,4 +24,10 @@ internal actual fun HazeSourceNode.clearHazeAreaLayerOnStop() {
       }
     }
   }
+}
+
+private tailrec fun Context.findActivityOrNull(): ComponentActivity? = when (this) {
+  is ComponentActivity -> this
+  is ContextWrapper -> baseContext.findActivityOrNull()
+  else -> null
 }
