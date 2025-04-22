712e803bd8ba48915c537075da4b9489a06362a1
Chris Banes <chris@banes.me>
2025-04-22 20:34:06 +0100

    Try using HardwareBuffer
    Investigate HW buffer latency (#608)

--- haze/src/androidMain/kotlin/dev/chrisbanes/haze/RenderScriptContext.kt
+++ haze/src/androidMain/kotlin/dev/chrisbanes/haze/RenderScriptContext.kt
@@ -7,17 +7,28 @@ package dev.chrisbanes.haze
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.ColorSpace
+import android.graphics.PixelFormat
+import android.hardware.HardwareBuffer
+import android.media.ImageReader
+import android.os.Build
+import android.os.Handler
+import android.os.HandlerThread
 import android.renderscript.Allocation
 import android.renderscript.Element
 import android.renderscript.RenderScript
 import android.renderscript.ScriptIntrinsicBlur
 import android.renderscript.Type
 import android.view.Surface
+import androidx.annotation.RequiresApi
 import androidx.compose.ui.unit.IntSize
 import androidx.core.graphics.createBitmap
+import kotlin.system.measureTimeMillis
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.trySendBlocking
+import kotlinx.coroutines.withContext
 
+@RequiresApi(Build.VERSION_CODES.Q)
 internal class RenderScriptContext(
   val context: Context,
   val size: IntSize,
@@ -30,10 +41,18 @@ internal class RenderScriptContext(
     get() = inputAlloc.surface
 
   private var outputAlloc: Allocation
-  var outputBitmap: Bitmap
-    private set
 
-  private val channel = Channel<Unit>(Channel.CONFLATED)
+  private var imageReader: ImageReader? = null
+
+  @field:Volatile
+  private var outBmp: Bitmap? = null
+  private val useHwBuffer: Boolean = true
+
+  private val colorSpace = ColorSpace.get(ColorSpace.Named.SRGB)
+
+  private val inputContentDrawnChannel = Channel<Unit>(Channel.CONFLATED)
+  private val outputDrawnChannel = Channel<Unit>(Channel.CONFLATED)
+  private val irHandlerThread = HandlerThread("ImageReaderHandlerThread").apply { start() }
 
   private var isDestroyed = false
 
@@ -41,37 +60,74 @@ internal class RenderScriptContext(
     val width = size.width.increaseToDivisor(4)
     val height = size.height.increaseToDivisor(4)
 
-    val type = Type.Builder(rs, Element.U8_4(rs)).setX(width).setY(height).create()
+    val type = Type.Builder(rs, Element.RGBA_8888(rs))
+      .setX(width)
+      .setY(height)
+      .create()
 
     val flags = Allocation.USAGE_SCRIPT or Allocation.USAGE_IO_INPUT
-
     inputAlloc = Allocation.createTyped(rs, type, flags)
-    inputAlloc.setOnBufferAvailableListener { allocation ->
-      if (!isDestroyed) {
-        allocation.ioReceive()
-        channel.trySendBlocking(Unit)
-      }
+    inputAlloc.setOnBufferAvailableListener { inputContentDrawnChannel.trySend(Unit) }
+    if (useHwBuffer) {
+      val imageReader = ImageReader.newInstance(
+        width,
+        height,
+        PixelFormat.RGBA_8888,
+        2,
+        HardwareBuffer.USAGE_GPU_SAMPLED_IMAGE,
+      )
+      imageReader.setOnImageAvailableListener(
+        { imageReader ->
+          val outputImage = imageReader.acquireLatestImage()
+          val buffer = outputImage.hardwareBuffer!!
+          outBmp = Bitmap.wrapHardwareBuffer(buffer, colorSpace)
+          buffer.close()
+          outputImage.close()
+          outputDrawnChannel.trySend(Unit)
+          HazeLogger.d(TAG) { "Hardware bitmap created" }
+        },
+        Handler(irHandlerThread.looper),
+      )
+      this.imageReader = imageReader
+      // output
+      val outputFlags = Allocation.USAGE_SCRIPT or Allocation.USAGE_IO_OUTPUT
+      outputAlloc = Allocation.createTyped(rs, type, outputFlags)
+      outputAlloc.surface = imageReader.surface
+    } else {
+      outBmp = createBitmap(width, height)
+      outputAlloc = Allocation.createFromBitmap(rs, outBmp)
     }
-
-    outputBitmap = createBitmap(width, height)
-    outputAlloc = Allocation.createFromBitmap(rs, outputBitmap)
 
     blurScript = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs))
     blurScript.setInput(inputAlloc)
   }
 
-  fun applyBlur(blurRadius: Float) {
-    if (isDestroyed) return
+  suspend fun process(blurRadius: Float): Bitmap? {
+    if (isDestroyed) return null
 
     blurScript.setRadius(blurRadius.coerceAtMost(25f))
-    blurScript.forEach(outputAlloc)
 
-    if (!isDestroyed) {
-      outputAlloc.copyTo(outputBitmap)
+    // Wait for the content to be written to the Surface
+    inputContentDrawnChannel.receive()
+
+    withContext(Dispatchers.Default) {
+      inputAlloc.ioReceive()
+      blurScript.forEach(outputAlloc)
+      if (useHwBuffer) {
+        val waitIoSendMs = measureTimeMillis { outputAlloc.ioSend() }
+        HazeLogger.d(TAG) { "Wait IO send: $waitIoSendMs ms" }
+        val waitIrResultMs = measureTimeMillis {
+          // wait ImageReader to receive and process the result
+          outputDrawnChannel.receive()
+        }
+        HazeLogger.d(TAG) { "Wait IR result: $waitIrResultMs ms" }
+      } else {
+        outputAlloc.copyTo(outBmp)
+      }
     }
+    if (isDestroyed) return null
+    return outBmp
   }
-
-  suspend fun awaitSurfaceWritten() = channel.receive()
 
   fun release() {
     HazeLogger.d(TAG) { "Release resources" }
@@ -79,7 +135,11 @@ internal class RenderScriptContext(
 
     blurScript.destroy()
     inputAlloc.destroy()
+
+    imageReader?.close()
     outputAlloc.destroy()
+    irHandlerThread.quitSafely()
+
     rs.destroy()
   }
 
