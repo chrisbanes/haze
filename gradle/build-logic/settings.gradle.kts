4f4d2cf2a449398a87d51e506d9a20a40945dea2
Chris Banes <chris@banes.me>
2025-08-07 21:27:24 +0100

    Fix build-logic settings repos

--- gradle/build-logic/settings.gradle.kts
+++ gradle/build-logic/settings.gradle.kts
@@ -1,19 +1,36 @@
 // Copyright 2023, Christopher Banes and the Tivi project contributors
 // SPDX-License-Identifier: Apache-2.0
 
+pluginManagement {
+  repositories {
+    google {
+      content {
+        includeGroupByRegex(".*google.*")
+        includeGroupByRegex(".*android.*")
+      }
+    }
+    mavenCentral()
+    gradlePluginPortal()
+  }
+}
 
 dependencyResolutionManagement {
-    repositories {
-        mavenCentral()
-        google()
-        gradlePluginPortal()
+  repositories {
+    google {
+      content {
+        includeGroupByRegex(".*google.*")
+        includeGroupByRegex(".*android.*")
+      }
     }
+    mavenCentral()
+    gradlePluginPortal()
+  }
 
-    versionCatalogs {
-        create("libs") {
-            from(files("../libs.versions.toml"))
-        }
+  versionCatalogs {
+    create("libs") {
+      from(files("../libs.versions.toml"))
     }
+  }
 }
 
 include(":convention")
