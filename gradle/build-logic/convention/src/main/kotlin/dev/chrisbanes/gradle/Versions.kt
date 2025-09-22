ce9e9a9b40e56a32f04f20e121bac991c4707f79
Chris Banes <chrisbanes@users.noreply.github.com>
2025-09-22 20:37:50 +0100

    Bump compile and min SDK versions (#773)
    Compile SDK = 36
    Min SDK = 23

--- gradle/build-logic/convention/src/main/kotlin/dev/chrisbanes/gradle/Versions.kt
+++ gradle/build-logic/convention/src/main/kotlin/dev/chrisbanes/gradle/Versions.kt
@@ -4,7 +4,7 @@
 package dev.chrisbanes.gradle
 
 object Versions {
-  const val COMPILE_SDK = 35
-  const val MIN_SDK = 21
-  const val TARGET_SDK = 35
+  const val COMPILE_SDK = 36
+  const val MIN_SDK = 23
+  const val TARGET_SDK = 36
 }
