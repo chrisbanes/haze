8e454b771cbe025cf9e108d518516f534dc59c2c
Chris Banes <chrisbanes@users.noreply.github.com>
2026-06-09 09:24:19 +0100

    Update to Android SDK 37 (#970)

--- gradle/build-logic/convention/src/main/kotlin/dev/chrisbanes/gradle/Versions.kt
+++ gradle/build-logic/convention/src/main/kotlin/dev/chrisbanes/gradle/Versions.kt
@@ -4,7 +4,7 @@
 package dev.chrisbanes.gradle
 
 object Versions {
-  const val COMPILE_SDK = 36
+  const val COMPILE_SDK = 37
   const val MIN_SDK = 23
-  const val TARGET_SDK = 36
+  const val TARGET_SDK = 37
 }
