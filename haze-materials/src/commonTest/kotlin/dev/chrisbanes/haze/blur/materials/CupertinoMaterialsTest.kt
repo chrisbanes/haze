44bb0c493bec54f54f9d002e90d93b9844279ba6
ardakazanci <nkflx2@gmail.com>
2026-07-27 21:18:17 +0300

    Add Cupertino material branch tests

--- haze-materials/src/commonTest/kotlin/dev/chrisbanes/haze/blur/materials/CupertinoMaterialsTest.kt
+++ haze-materials/src/commonTest/kotlin/dev/chrisbanes/haze/blur/materials/CupertinoMaterialsTest.kt
@@ -3,9 +3,11 @@
 
 package dev.chrisbanes.haze.blur.materials
 
+import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import dev.chrisbanes.haze.blur.HazeColorEffect
 import kotlin.test.Test
 
 class CupertinoMaterialsTest {
@@ -23,5 +25,47 @@ class CupertinoMaterialsTest {
     )
 
     assertThat(style.backgroundColor).isEqualTo(containerColor)
+  }
+
+  @Test
+  fun cupertinoMaterialStyle_usesLightEffectsForLightContainer() {
+    val lightBackgroundColor = Color(0xFF111111)
+    val lightForegroundColor = Color(0xFF222222)
+
+    val style = cupertinoMaterialStyle(
+      containerColor = Color.White,
+      lightBackgroundColor = lightBackgroundColor,
+      lightForegroundColor = lightForegroundColor,
+      darkBackgroundColor = Color(0xFF333333),
+      darkForegroundColor = Color(0xFF444444),
+    )
+
+    assertThat(style.colorEffects).isEqualTo(
+      listOf(
+        HazeColorEffect.tint(lightBackgroundColor, BlendMode.ColorDodge),
+        HazeColorEffect.tint(lightForegroundColor),
+      ),
+    )
+  }
+
+  @Test
+  fun cupertinoMaterialStyle_usesDarkEffectsForDarkContainer() {
+    val darkBackgroundColor = Color(0xFF333333)
+    val darkForegroundColor = Color(0xFF444444)
+
+    val style = cupertinoMaterialStyle(
+      containerColor = Color.Black,
+      lightBackgroundColor = Color(0xFF111111),
+      lightForegroundColor = Color(0xFF222222),
+      darkBackgroundColor = darkBackgroundColor,
+      darkForegroundColor = darkForegroundColor,
+    )
+
+    assertThat(style.colorEffects).isEqualTo(
+      listOf(
+        HazeColorEffect.tint(darkBackgroundColor, BlendMode.Overlay),
+        HazeColorEffect.tint(darkForegroundColor),
+      ),
+    )
   }
 }
