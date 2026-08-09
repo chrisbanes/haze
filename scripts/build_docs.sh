61f633e1d7ba805f6294cf57c4e137827fa11891
Chris Banes <chris@banes.me>
2026-08-09 19:15:20 +0100

    Fetch published snapshot metadata

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -eu
+
 ./gradlew \
     --no-scan \
     :internal:dokka:dokkaGenerate \
@@ -16,22 +18,9 @@ cp -r sample/web/build/dist/js/productionExecutable/ docs/sample/js/
 
 cp CHANGELOG.md docs/changelog.md
 
-snapshot_version="$(sed -n 's/^VERSION_NAME=//p' gradle.properties)"
-case "$snapshot_version" in
-  ''|*[!0-9A-Za-z._+-]*)
-    echo "Invalid VERSION_NAME: $snapshot_version"
-    exit 1
-    ;;
-esac
-
 mkdir -p docs/javascripts
-printf '%s\n' \
-  '<?xml version="1.0" encoding="UTF-8"?>' \
-  '<metadata>' \
-  '  <versioning>' \
-  "    <latest>$snapshot_version</latest>" \
-  '  </versioning>' \
-  '</metadata>' \
-  > docs/javascripts/snapshot-metadata.xml
+curl --fail --location --silent --show-error \
+  --output docs/javascripts/snapshot-metadata.xml \
+  https://central.sonatype.com/repository/maven-snapshots/dev/chrisbanes/haze/haze/maven-metadata.xml
 
 mkdocs $@
