56732d44fd8c304a6b00630653f22c815936c290
Chris Banes <chris@banes.me>
2026-08-27 10:44:34 +0100

    Configure remaining web test executables

--- haze-materials/build.gradle.kts
+++ haze-materials/build.gradle.kts
@@ -19,6 +19,13 @@ android {
 
 kotlin {
   addDefaultHazeTargets()
+  @OptIn(org.jetbrains.kotlin.gradle.ExperimentalWasmDsl::class)
+  wasmJs {
+    binaries.executable()
+  }
+  js {
+    binaries.executable()
+  }
   explicitApi()
 
   sourceSets {
