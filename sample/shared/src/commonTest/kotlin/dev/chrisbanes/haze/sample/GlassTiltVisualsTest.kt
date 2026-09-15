e47ee529da903f1fa1503b33be02ad97c73d69e8
Chris Banes <chris@banes.me>
2026-09-15 11:32:28 +0100

    Remove invalid Glass Tilt style test

--- sample/shared/src/commonTest/kotlin/dev/chrisbanes/haze/sample/GlassTiltVisualsTest.kt
+++ sample/shared/src/commonTest/kotlin/dev/chrisbanes/haze/sample/GlassTiltVisualsTest.kt
@@ -5,25 +5,19 @@
 
 package dev.chrisbanes.haze.sample
 
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.snapshots.Snapshot
 import androidx.compose.runtime.snapshots.SnapshotStateObserver
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.test.ExperimentalTestApi
-import androidx.compose.ui.test.v2.runComposeUiTest
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.LayoutDirection
 import assertk.assertThat
 import assertk.assertions.isEqualTo
-import assertk.assertions.isSameInstanceAs
 import assertk.assertions.isTrue
 import dev.chrisbanes.haze.ExperimentalHazeApi
-import dev.chrisbanes.haze.glass.GlassStyle
 import kotlin.test.Test
 
-@OptIn(ExperimentalTestApi::class)
 class GlassTiltVisualsTest {
   @Test
   fun lightAlignment_tracksTheLatestPositionInAbsoluteCoordinates() {
@@ -50,25 +44,4 @@ class GlassTiltVisualsTest {
     }
   }
 
-  @Test
-  fun rememberedStyle_survivesTiltPositionUpdates() = runComposeUiTest {
-    val position = mutableStateOf(Offset(0.50f, 0.50f))
-    var firstStyle: GlassStyle? = null
-    var latestStyle: GlassStyle? = null
-
-    setContent {
-      val style = rememberGlassTiltStyle(position)
-      GlassTiltLightPositionLabel(position)
-      SideEffect {
-        if (firstStyle == null) firstStyle = style
-        latestStyle = style
-      }
-    }
-    waitForIdle()
-
-    runOnIdle { position.value = Offset(0.68f, 0.32f) }
-    waitForIdle()
-
-    assertThat(latestStyle).isSameInstanceAs(firstStyle)
-  }
 }
